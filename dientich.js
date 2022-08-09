ca827be22b68b2ca8df0725dae81ecee84a6854c
dung1993 <crorstar1704@gmail.com>
2022-08-09 16:41:59 +0700

    dientich.html

---
+++ dientich.js
@@ -0,0 +1,6 @@
+let inputheight = prompt("height")
+let inputwidth = prompt("width")
+let height = parseInt(inputheight)
+let width = parseInt(inputwidth)
+let area = height*width
+document.write('area ='+ area)
